95dda998fced9e62ea34dd1e34b34771d36009dc
iahmedani <imran.ahmedani@gmail.com>
2018-04-14 10:04:35 +0500

    seeting up env variables

---
+++ config/settings.js
@@ -0,0 +1,13 @@
+exports.lConfig = {
+    server:'localhost',
+    user:'sa',
+    database:'acf',
+    password:'imran123'
+}
+
+exports.cloudConfig = {
+    server:'45.76.90.167',
+    user:'sa',
+    database:'acf',
+    password:'7Cd29FhP2YQU4FtP'
+}
